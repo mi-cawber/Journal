e617c90d0b877a9b26ef23f15819851e70b599ab
mi-cawber <106614898+mi-cawber@users.noreply.github.com>
2024-11-18 18:03:26 -0500

    cleaning

---
+++ src/DataEntry.java
@@ -0,0 +1,87 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DataEntry {
+
+    /**
+     * This method allows user to enter hours of sleep for the last night,
+     * and records this on a .csv file, along with the current date.
+     * @throws Exception
+     */
+    static void enterSleep() throws Exception {
+        //print writer object
+        PrintWriter sleepWriter = new PrintWriter(new FileWriter("sleep.csv", true));
+        //new scanner
+        Scanner sc = new Scanner(System.in);
+        //capture date
+        String date = String.valueOf(LocalDate.now());
+
+        //enter sleep
+        System.out.print("Sleep (Hours): ");
+        double doubleInput = sc.nextDouble();
+        //print into separate columns
+        sleepWriter.println(doubleInput + " , " + date);
+        //needed to flush buffer
+        sleepWriter.close();
+    }
+
+    /**
+     * This method allows user to enter a mood score for the day,
+     * and records this on a .csv file, along with the current date.
+     * @throws Exception
+     */
+    static void enterMood() throws Exception {
+        PrintWriter moodWriter = new PrintWriter(new FileWriter("mood.csv", true));
+        //new scanner
+        Scanner sc = new Scanner(System.in);
+        //capture date
+        String date = String.valueOf(LocalDate.now());
+
+        //enter mood
+        System.out.print("Mood (1-10): ");
+        double doubleInput = sc.nextDouble();
+        moodWriter.println(doubleInput + " , " + date);
+        //needed to flush buffer
+        moodWriter.close();
+    }
+
+    /**
+     * This method allows user to enter mgs of caffeine ingested for the day,
+     * and records this on a .csv file, along with the current date.
+     * @throws Exception
+     */
+    static void enterCaffeine() throws Exception{
+        PrintWriter caffeineWriter = new PrintWriter(new FileWriter("caffeine.csv", true));
+        //new scanner
+        Scanner sc = new Scanner(System.in);
+        //capture date
+        String date = String.valueOf(LocalDate.now());
+
+        //enter caffeine
+        System.out.print("Caffeine (Milligrams): ");
+        double doubleInput = sc.nextDouble();
+        caffeineWriter.println(doubleInput + " , " + date); //write mgs
+        caffeineWriter.close();
+    }
+
+    /**
+     * This method allows user to enter whether they exercised that day,
+     * and records this on a .csv file, along with the current date.
+     * @throws Exception
+     */
+    static void enterWorkout() throws Exception{
+        PrintWriter workoutWriter = new PrintWriter(new FileWriter("workout.csv", true));
+        //new scanner
+        Scanner sc = new Scanner(System.in);
+        //capture date
+        String date = String.valueOf(LocalDate.now());
+
+        //enter workout
+        System.out.print("Workout?\n(0) No\n(1) Yes\n");
+        int intInput = sc.nextInt();
+        workoutWriter.println(intInput + " , " + date);
+        workoutWriter.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,7 @@ public class Main {
         //new scanner, comp object
         Scanner sc = new Scanner(System.in);
         Computation comp = new Computation();
+        DataEntry data = new DataEntry();
 
 
         //controller for while loop
@@ -40,10 +41,10 @@ public class Main {
             if (decisionInput.equals("1")) { //enter inputs
 
                 //enter all relevant values
-                enterSleep();
-                enterMood();
-                enterCaffeine();
-                enterWorkout();
+                data.enterSleep();
+                data.enterMood();
+                data.enterCaffeine();
+                data.enterWorkout();
 
                 //+1 to days collected tracker
                 comp.addDay();
@@ -64,86 +65,5 @@ public class Main {
                 decisionPath = true;
             }
         }
-    }
-
-
-    /**
-     * This method allows user to enter hours of sleep for the last night,
-     * and records this on a .csv file, along with the current date.
-     * @throws Exception
-     */
-    static void enterSleep() throws Exception {
-        //print writer object
-        PrintWriter sleepWriter = new PrintWriter(new FileWriter("sleep.csv", true));
-        //new scanner
-        Scanner sc = new Scanner(System.in);
-        //capture date
-        String date = String.valueOf(LocalDate.now());
-
-        //enter sleep
-        System.out.print("Sleep (Hours): ");
-        double doubleInput = sc.nextDouble();
-        //print into separate columns
-        sleepWriter.println(doubleInput + " , " + date);
-        //needed to flush buffer
-        sleepWriter.close();
-    }
-
-    /**
-     * This method allows user to enter a mood score for the day,
-     * and records this on a .csv file, along with the current date.
-     * @throws Exception
-     */
-    static void enterMood() throws Exception {
-        PrintWriter moodWriter = new PrintWriter(new FileWriter("mood.csv", true));
-        //new scanner
-        Scanner sc = new Scanner(System.in);
-        //capture date
-        String date = String.valueOf(LocalDate.now());
-
-        //enter mood
-        System.out.print("Mood (1-10): ");
-        double doubleInput = sc.nextDouble();
-        moodWriter.println(doubleInput + " , " + date);
-        //needed to flush buffer
-        moodWriter.close();
-    }
-
-    /**
-     * This method allows user to enter mgs of caffeine ingested for the day,
-     * and records this on a .csv file, along with the current date.
-     * @throws Exception
-     */
-    static void enterCaffeine() throws Exception{
-        PrintWriter caffeineWriter = new PrintWriter(new FileWriter("caffeine.csv", true));
-        //new scanner
-        Scanner sc = new Scanner(System.in);
-        //capture date
-        String date = String.valueOf(LocalDate.now());
-
-        //enter caffeine
-        System.out.print("Caffeine (Milligrams): ");
-        double doubleInput = sc.nextDouble();
-        caffeineWriter.println(doubleInput + " , " + date); //write mgs
-        caffeineWriter.close();
-    }
-
-    /**
-     * This method allows user to enter whether they exercised that day,
-     * and records this on a .csv file, along with the current date.
-     * @throws Exception
-     */
-    static void enterWorkout() throws Exception{
-        PrintWriter workoutWriter = new PrintWriter(new FileWriter("workout.csv", true));
-        //new scanner
-        Scanner sc = new Scanner(System.in);
-        //capture date
-        String date = String.valueOf(LocalDate.now());
-
-        //enter workout
-        System.out.print("Workout?\n(0) No\n(1) Yes\n");
-        int intInput = sc.nextInt();
-        workoutWriter.println(intInput + " , " + date);
-        workoutWriter.close();
     }
 }
